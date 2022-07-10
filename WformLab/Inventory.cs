fe9fe14eae5d917323ec5237ee1c3becfeff3c63
gedion24 <57101143+gedion24@users.noreply.github.com>
2022-07-10 10:55:26 +0300

    commit

---
+++ WformLab/Inventory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WformLab.model;
+
+namespace WformLab
+{
+    public partial class Inventory : Form
+    {
+        public Inventory()
+        {
+            InitializeComponent();
+        }
+
+        private void Inventory_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+           // string number = txtnum.Text;
+          //  string date = txtdate.Text;
+          //  string inventorynumber = txtinventory.Text;
+          //  string objname = txtobjname.Text;
+          //  string count = txtcount.Text;
+          //  string price = txtcprice.Text;
+
+            Item it = new Item();
+            it.number = txtnum.Text;
+            it.date = txtdate.Text;
+            it.inventorynumber = txtinventory.Text;
+            it.objectname = txtobjname.Text;
+            it.count = int.Parse (txtcount.Text);
+            it.price = txtcprice.Text;
+
+            MessageBox.Show("added");
+
+
+
+
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ WformLab/model/model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WformLab.model
+{
+    class Item
+    {
+        public string number { get; set; }
+        public string date { get; set; }
+        public string inventorynumber { get; set; }
+        public string objectname { get; set; }
+        public int count { get; set; }
+        public string price { get; set; }
+
+        public void save()
+        {
+            Console.WriteLine("save");
+        }
+
+    }
+}
